d6c6c2ffd6c5a6b4ffe58f093cce706b5c55e234
Rhio Kim <rhio.kim@gmail.com>
2021-12-04 19:06:23 +0900

    feat: create page.id indexted snapshot & updateDiff

--- mod.ts
+++ mod.ts
@@ -5,10 +5,22 @@ import {
   APIErrorCode,
   Client,
 } from "https://deno.land/x/notion_sdk/src/mod.ts";
+import { updatedDiff } from 'https://esm.sh/deep-object-diff'
+
 
 const notion = new Client({
   auth: Deno.env.get("NOTION_TOKEN"),
 });
+
+/**
+ * How to unwrap type of Promise<T>?
+ * https://stackoverflow.com/questions/48011353/how-to-unwrap-the-type-of-a-promise */
+type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
+// let snapshot: Awaited<ReturnType<typeof notion.databases.query>>['results'] = []
+let snapshot: {
+  [key: string] : Awaited<ReturnType<typeof notion.databases.query>>['results'][0]
+} = {
+}
 
 try {
   const dbId = Deno.env.get("NOTION_DATABASE_ID");
@@ -16,11 +28,24 @@ try {
     throw Error("NO NOTION DATABASE ENVIRONMENT ID");
   }
 
-  const page = await notion.databases.query({
+  const { results, next_cursor } = await notion.databases.query({
     database_id: dbId,
   });
+
+  const indexedPages = results.reduce((accu, p) => ({
+    ...accu,
+    [p.id]: p
+  }), {})
+  // const diff = updatedDiff(snapshot, results)
+  const diff = updatedDiff(snapshot, indexedPages)
+  console.log('diff:',diff)
+  console.log('result:', indexedPages)
+
+  snapshot = indexedPages
+
 } catch (error) {
   if (error.code === APIErrorCode.ObjectNotFound) {
+    console.log(error)
   } else {
     console.error(error);
   }
